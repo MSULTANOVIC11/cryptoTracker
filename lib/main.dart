2014d31f99e1e5fec52075564f4ff08be20e61ad
MSD11 <msd11@autistici.org>
2020-03-09 21:24:51 +0100

    add crypto icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
+import 'package:crypto_font_icons/crypto_font_icons.dart';
 import 'package:flutter/material.dart';
 import 'api.dart';
 import 'dart:async';
-import 'dart:convert';
 
 void main() => runApp(CryptoTracker());
 
@@ -14,7 +14,7 @@ class CryptoTracker extends StatelessWidget {
         appBar: AppBar(title: Text("CryptoCurrency"), actions: <Widget>[
           IconButton(
             icon: Icon(Icons.refresh),
-            onPressed: () {}, //faire raffraichir la page
+            onPressed: () {}, //refresh page
           )
         ]),
         body: SafeArea(
@@ -62,6 +62,189 @@ class _CryptoPage extends State<CryptoPage> {
 }
 
 Widget listViewWidget(List<dynamic> data) {
+  IconData iconPick(String name) {
+    switch (name) {
+      case 'ADC':
+        return CryptoFontIcons.ADC;
+      case 'AEON':
+        return CryptoFontIcons.AEON;
+      case 'AMP':
+        return CryptoFontIcons.AMP;
+      case 'ANC':
+        return CryptoFontIcons.ANC;
+      case 'ARDR':
+        return CryptoFontIcons.ARDR;
+      case 'AUR':
+        return CryptoFontIcons.AUR;
+      case 'BAY':
+        return CryptoFontIcons.BAY;
+      case 'BCN':
+        return CryptoFontIcons.BCN;
+      case 'BRK':
+        return CryptoFontIcons.BRK;
+      case 'BRX':
+        return CryptoFontIcons.BRX;
+      case 'BSD':
+        return CryptoFontIcons.BSD;
+      case 'BTA':
+        return CryptoFontIcons.BTA;
+      case 'BTC':
+        return CryptoFontIcons.BTC;
+      case 'BTC_ALT':
+        return CryptoFontIcons.BTC_ALT;
+      case 'BTCD':
+        return CryptoFontIcons.BTCD;
+      case 'BTS':
+        return CryptoFontIcons.BTS;
+      case 'CLAM':
+        return CryptoFontIcons.CLAM;
+      case 'CLOAK':
+        return CryptoFontIcons.CLOAK;
+      case 'DASH':
+        return CryptoFontIcons.DASH;
+      case 'DCR':
+        return CryptoFontIcons.DCR;
+      case 'DGB':
+        return CryptoFontIcons.DGB;
+      case 'DGD':
+        return CryptoFontIcons.DGD;
+      case 'DGX':
+        return CryptoFontIcons.DGX;
+      case 'DMD':
+        return CryptoFontIcons.DMD;
+      case 'DOGE':
+        return CryptoFontIcons.DOGE;
+      case 'EMC':
+        return CryptoFontIcons.EMC;
+      case 'ERC':
+        return CryptoFontIcons.ERC;
+      case 'ETC':
+        return CryptoFontIcons.ETC;
+      case 'ETH':
+        return CryptoFontIcons.ETH;
+      case 'FCT':
+        return CryptoFontIcons.FCT;
+      case 'FLO':
+        return CryptoFontIcons.FLO;
+      case 'FRK':
+        return CryptoFontIcons.FRK;
+      case 'FTC':
+        return CryptoFontIcons.FTC;
+      case 'GAME':
+        return CryptoFontIcons.GAME;
+      case 'GLD':
+        return CryptoFontIcons.GLD;
+      case 'GNT':
+        return CryptoFontIcons.GNT;
+      case 'GRC':
+        return CryptoFontIcons.GRC;
+      case 'GRS':
+        return CryptoFontIcons.GRS;
+      case 'HEAT':
+        return CryptoFontIcons.HEAT;
+      case 'ICN':
+        return CryptoFontIcons.ICN;
+      case 'IFC':
+        return CryptoFontIcons.IFC;
+      case 'INCNT':
+        return CryptoFontIcons.INCNT;
+      case 'IOC':
+        return CryptoFontIcons.IOC;
+      case 'KMD':
+        return CryptoFontIcons.KMD;
+      case 'KOBO':
+        return CryptoFontIcons.KOBO;
+      case 'KORE':
+        return CryptoFontIcons.KORE;
+      case 'LBC':
+        return CryptoFontIcons.LBC;
+      case 'LDOGE':
+        return CryptoFontIcons.LDOGE;
+      case 'LSK':
+        return CryptoFontIcons.LSK;
+      case 'LTC':
+        return CryptoFontIcons.LTC;
+      case 'MAID':
+        return CryptoFontIcons.MAID;
+      case 'MINT':
+        return CryptoFontIcons.MINT;
+      case 'MONA':
+        return CryptoFontIcons.MONA;
+      case 'MUE':
+        return CryptoFontIcons.MUE;
+      case 'NEOS':
+        return CryptoFontIcons.NEOS;
+      case 'NLG':
+        return CryptoFontIcons.NLG;
+      case 'NMC':
+        return CryptoFontIcons.NMC;
+      case 'NOTE':
+        return CryptoFontIcons.NOTE;
+      case 'NUC':
+        return CryptoFontIcons.NUC;
+      case 'NXT':
+        return CryptoFontIcons.NXT;
+      case 'OK':
+        return CryptoFontIcons.OK;
+      case 'OMNI':
+        return CryptoFontIcons.OMNI;
+      case 'PINK':
+        return CryptoFontIcons.PINK;
+      case 'PIVX':
+        return CryptoFontIcons.PIVX;
+      case 'POT':
+        return CryptoFontIcons.POT;
+      case 'PPC':
+        return CryptoFontIcons.PPC;
+      case 'QRK':
+        return CryptoFontIcons.QRK;
+      case 'RBY':
+        return CryptoFontIcons.RBY;
+      case 'RDD':
+        return CryptoFontIcons.RDD;
+      case 'REP':
+        return CryptoFontIcons.REP;
+      case 'RISE':
+        return CryptoFontIcons.RISE;
+      case 'SJCX':
+        return CryptoFontIcons.SJCX;
+      case 'SLS':
+        return CryptoFontIcons.SLS;
+      case 'STEEM':
+        return CryptoFontIcons.STEEM;
+      case 'STRAT':
+        return CryptoFontIcons.STRAT;
+      case 'SYS':
+        return CryptoFontIcons.SYS;
+      case 'TRIG':
+        return CryptoFontIcons.TRIG;
+      case 'UBQ':
+        return CryptoFontIcons.UBQ;
+      case 'UNITY':
+        return CryptoFontIcons.UNITY;
+      case 'USDT':
+        return CryptoFontIcons.USDT;
+      case 'VRC':
+        return CryptoFontIcons.VRC;
+      case 'VTC':
+        return CryptoFontIcons.VTC;
+      case 'WAVES':
+        return CryptoFontIcons.WAVES;
+      case 'XCP':
+        return CryptoFontIcons.XCP;
+      case 'XEM':
+        return CryptoFontIcons.XEM;
+      case 'XMR':
+        return CryptoFontIcons.XMR;
+      case 'XRP':
+        return CryptoFontIcons.XRP;
+      case 'ZEC':
+        return CryptoFontIcons.ZEC;
+      default:
+        return Icons.public;
+    }
+  }
+
   return Container(
     child: ListView.builder(
         itemCount: 20,
@@ -69,6 +252,7 @@ Widget listViewWidget(List<dynamic> data) {
         itemBuilder: (context, position) {
           return Card(
             child: ListTile(
+              leading: Icon(iconPick(data[position]['symbol'])),
               title: Text(
                 data[position]['name'],
                 style: TextStyle(
@@ -82,9 +266,6 @@ Widget listViewWidget(List<dynamic> data) {
                     " %",
                 style: percentChange(
                     data[position]['quote']['USD']['percent_change_24h']),
-              ),
-              leading: Padding(
-                padding: const EdgeInsets.all(8.0),
               ),
               subtitle: Text(
                 data[position]['quote']['USD']['price'].toString() + " \$",
